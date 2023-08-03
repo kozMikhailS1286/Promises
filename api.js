0fee7939589d74a841d209ad0986ae3989449e91
Mikhail <109968626+kozMikhailS1286@users.noreply.github.com>
2023-08-03 23:30:49 +0300

    Add files via upload

---
+++ api.js
@@ -0,0 +1,12 @@
+const api = {
+    sendStudentsCountToItKamasutra(studentsCount) {
+        return axios.post("https://it-kamasutra.com/students-need", 
+        { count: studentsCount });
+    },
+    getVacanciesCountFromMicrosoft() {
+        return axios.get("https://microsoft.com").then(res=>res.data.vacancies);
+    },
+    getVacanciesCountFromGoogle() {
+        return axios.get("https://google.com").then(res=>res.data.vacancies);;
+    }
+}
